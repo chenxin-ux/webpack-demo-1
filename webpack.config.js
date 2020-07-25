427be4263a6574a67948639d4cb113d585765e19
chenxin <1249148700@qq.com>
2020-07-25 16:43:05 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "xdml",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
